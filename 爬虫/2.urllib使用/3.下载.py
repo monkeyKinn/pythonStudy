4878345ab9c1bc0da34de2373a267da53a060830
JinShengcong <752724895@qq.com>
2022-10-29 01:35:26 +0800

    爬虫概念+urllib基本使用+一个类型六个方法+下载+请求对象的定制
    更新笔记+日记
    
    2022年10月26日 22:33:28

--- 爬虫/2.urllib使用/3.下载.py
+++ 爬虫/2.urllib使用/3.下载.py
@@ -3,6 +3,7 @@
 # @Author : 金圣聪
 # @File : 3.下载
 # @Project : pythonStudy
+import traceback
 import urllib.request
 
 url = 'http://www.baidu.com'
@@ -18,9 +19,13 @@ print(tuple_resp)
 
 # 2.图片下载
 img_url = "https://pbs.twimg.com/media/FgJRtvKUYAEMbKH?format=jpg&name=large"
-tuple_resp_img = urllib.request.urlretrieve(filename='yua_mikami.jpg', url=img_url)
-# ('yua_mikami.jpg', <http.client.HTTPMessage object at 0x000001E53C061460>)
-print(tuple_resp_img)
+try:
+    tuple_resp_img = urllib.request.urlretrieve(filename='yua_mikami.jpg', url=img_url)
+    # ('yua_mikami.jpg', <http.client.HTTPMessage object at 0x000001E53C061460>)
+    print(tuple_resp_img)
+except Exception as e:
+    print(f'problem is {e}')
+    traceback.print_exc()
 
 # 3.视频下载
 view_url = 'https://vd2.bdstatic.com/mda-njqpsgtjp53qbvc3/cae_h264/1666722546608557255/mda-njqpsgtjp53qbvc3.mp4?v_from_s=hkapp-haokan-nanjing&auth_key=1666971142-0-0-6d36a7a79b24bbc9c2b961f9961a4abc&bcevod_channel=searchbox_feed&pd=1&cd=0&pt=3&logid=0141935138&vid=7973159718388296463&abtest=104959_1-105227_2&klogid=0141935138'
