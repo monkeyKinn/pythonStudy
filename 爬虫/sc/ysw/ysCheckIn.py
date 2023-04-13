adc8cc0199e256ca7579a8af136b916990f3123c
JinShengcong <752724895@qq.com>
2023-04-14 00:37:35 +0800

    随便写点代挂
    2023.04.14

---
+++ 爬虫/sc/ysw/ysCheckIn.py
@@ -0,0 +1,58 @@
+# _*_ coding : utf-8 _*_
+# ysw 签到
+# @Time : 2023/4/13 21:36
+# @Author : 金圣聪
+# @File : ysCheckIn.py
+# @Project : pythonStudy
+import requests
+import os
+import time
+import random
+
+
+def getCookie():
+    try:
+        if "YSW_TOKEN" in os.environ:
+            if len(os.environ["YSW_TOKEN"]) > 10:
+                token = os.environ["YSW_TOKEN"]
+                print("\n当前从环境变量获取YSW_TOKEN\n")
+                return token
+    except Exception as e:
+        print(f"【getTOKEN Error】{e}")
+
+cookieList = getCookie().split('&')
+print(cookieList)
+
+def send(tokenParams):
+    headers = {
+        'authority': 'api.codeleven.cn',
+        'accept': '*/*',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'no-cache',
+        'origin': 'https://live.yuanshuwang.com',
+        'pragma': 'no-cache',
+        'referer': 'https://live.yuanshuwang.com/',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        # 这个是动态的
+        'user-token': tokenParams
+    }
+
+    params = {
+        'taskCode': 'ab001',
+    }
+
+    # 随机睡几秒
+    time.sleep(round(random.uniform(1, 10), 3))
+    # 签到
+    response = requests.post(
+        'https://api.codeleven.cn/nft-live/server/integral/user/receive/integral',
+        params=params,
+        headers=headers,
+    )
+    print('签到成功' if len(response.json().get('message')) == 0 else response.json().get('message'))
+
+for cookie in cookieList:
+    send(cookie)
