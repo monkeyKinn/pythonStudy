ee4a0e11cbbc8d65ff719aed20d82fb8b030aa64
JinShengcong <752724895@qq.com>
2023-05-17 00:32:14 +0800

    ur g
    2023.05.16

--- 爬虫/其他课/未知/sc/ur/buying.py
+++ 爬虫/其他课/未知/sc/ur/buying.py
@@ -9,10 +9,11 @@ import time
 cookies = {
     'acw_tc': '7ae4079c16842518773358230e7eceeb35528daab734810cfa88871dc1',
     'cdn_sec_tc': '7ae4079c16842518773358230e7eceeb35528daab734810cfa88871dc1',
-    'acw_sc__v2': '6463a4e5fa61adbf4cf25421144044208d66f571',
     'PHPSESSID': 'k6kqn4kp3gj93tg3fosbdk4cth',
-    'ssxmod_itna': 'QqGxBDRDcDy7D=Ye0L5YIEP7qCO=KCe7b3ODlOO4A5D8D6DQeGTTXNKeTYed5QvWi70mxQWCbwKGKrfeYhTSADB3DEx06xW9QxiiuDCeDIDWeDiDGRQDFxYoDeg9dDFtyt/luhxWKDwDB=DmqG2BMQDm4DfDDLu3YvuWnfuGu3xgwiDAkDBj0+uD0tDIqGXWAW4tqDBQPa4jqUtVeGWtoQDxeGuDG6q1qd4x0Pc0TtXA1v5txh5I73qzFf+DY5d0hxrY2YKAzAdBSgQzKGIj7mQYxxxD',
-    'ssxmod_itna2': 'QqGxBDRDcDy7D=Ye0L5YIEP7qCO=KCe7b3Dnxn9G9AKDsKFDLGQQu0AxmuMGlIQKUlhGGQ1UniwhQqUWiTEu5nA+Wo4I4O+GfTUY8/lvUGoV2eENdh1SuGg0p15WlQLU4qcgj04=twTD8vPixidqC2belGdKtTh+xAWUDYT4bImnYHpk8IrbCmdZgwihamAiFIii4vPeQSNpIyNxKMC1febKrcdKguUnlgNVDMGQeC7P9E6GfgQoNQn1tLuxsTvCSls4wgkWLozlfoWCTErMcoWxHUwyEOkOHQuRlRweV3Z/3lcbHL/Gd7/0zZrWku1R7=Fiz3Dh+dhIgzL27imGinobWTsMz2MH+3w4gzzxhDqCCkwuDpeqT1jknqoyuYCLOLehD4xF0eD7QdlG5QG7OxdWKeAxVGE=nvKYqPGDqn0F2ew4oKDWiOwsjGGlGKeL8ipxADpCxLbweaQfWAeDdbxvs+Ps9oCiq4Aed0DPlD/lqZRW4C34W9+UI50PtDWp4pK7eq0qDjKDeu79B4wSNY7W/+q=bAF0f1aA30fjOWXbihUnh6bia5VrAjb57/Djcnab5CFARtjYTDD=',
+    'acw_sc__v2': '6463ac0a337330a7954a199374532ecdcda07c53',
+    'arp_scroll_position': '82',
+    'ssxmod_itna': 'QqGxBDRDcDy7D=Ye0L5YIEP7qCO=KCe7F58ODlOEQxA5D8D6DQeGTT2NKeTYed57GbxhxTK0ix3=8e+WoThei2w4GLDmKDyKiOueDxaq0rD74irDDxD3cD7PGmDieC0D7oZMgcTNKiOD7eDXxGCDQIsExGWDiPD7pFxMmKw7pIC7rAsK4D1qYvPFmKD9x0CDlp4vbi8DD5ffbDzRZOADm43woAGDCKDjpYC8OYDU0IznxI6ZlD3Yl0eaOinW1eDg7htQY4KYGeqFHnPzarKrqiTpibyYYD==',
+    'ssxmod_itna2': 'QqGxBDRDcDy7D=Ye0L5YIEP7qCO=KCe7F58Dnxn9G9AKDsYqYDLAQiwiliDmwcGHD8246bK6QjAPKnGrYH=KXGFGIxfnKAGFXjqQik86xZSKTLK2AH7m0L+Y4Nt5nEdsCzOrXi=sHjcjMfjRqdAQld4rlQAm3L8Sd5x=f23v4f+tjR=cQLBzBuU0mTpn+bCl+YvYCc032WdfxKhfhEufcYCAc9Y545GFTYGh4La=Ta/AjoovuWIaCTCY1EyRnor=UACkBH8eb=7PShlfZ58Y5KUiAoj4ka7gb=j3V2lRq0x8+UdjeQVycHbPm05m0rviFjzn20pQ/9q8G=Sx4GwwIHPcRwjirS5F49qWwHtRqI9d8beYnod9dBRDRwHRuD34G8wHDIuRnDyqp4wm/qC7cIB+njWxOmm3OaFnEY3jW3Ef2IznceHC0KaO0oD07FGCx2rkiQb0=xothpxw=eiSjw6enm6brbYDUUoWIa=A=fipUZDKyajraUncDXPXw65376K9FeGDD7=DYIcmgPGqIABKA45jMKV8y9B=A+jGToteAoZFAb+6iN0nKXueE+xzuKNDaSxv5=x94qShDD==',
 }
 
 headers = {
@@ -38,7 +39,7 @@ json_data = {
 
 while True:
     try:
-        time.sleep(0.8)
+        time.sleep(0.001)
         response = requests.post(
             'https://ur.himayi.cn/h5/super/api/member/getDetailsSealList',
             cookies=cookies,
@@ -46,12 +47,11 @@ while True:
             json=json_data,
         )
         goods = response.json().get('data').get('data')
-        print(len(goods))
         if len(goods) >= 1:
             for good in goods:
+                it_id = good.get('id')
                 if good.get('goods_status') != 2:
                     print('有可锁的')
-                    it_id = good.get('id')
                     json_data_creat = {
                         'goods_id': 14,
                         'order_type': 3,
@@ -64,17 +64,19 @@ while True:
                         print(response.json())
                         flag = response.json().get('msg') == '点击的太快了呢,请您稍后重试!'
                         while flag:
-                            time.sleep(0.3)
+                            time.sleep(0.001)
                             response = requests.post('https://ur.himayi.cn/h5/super/order/createOrder', cookies=cookies,
                                                      headers=headers,
                                                      json=json_data_creat)
                             flag = response.json().get('msg') == '点击的太快了呢,请您稍后重试!'
                             if not flag:
-                                print(response.json())
+                                print(response.text)
                     except Exception as e:
                         print(e)
                         print(f'小错误:{e}')
-                print('没有未锁定的')
+                print(f'没有未锁定的,id为{it_id}')
+        else:
+            print('已退市')
     except Exception as e:
         print(f'大错误:{e}')
 # Note: json_data will not be serialized by requests
